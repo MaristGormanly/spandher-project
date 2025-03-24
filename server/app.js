7f2d815d7d673aa952c27e534c27067b5649acfe
taj106 <sirtaj.pandher1@marist.edu>
2025-03-24 15:42:12 -0400

    website click works

---
+++ server/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 1337;
+
+app.get('/', function (req, res) {
+    res.sendFile('index.html', { root: './client/views' });
+});
+
+app.listen(port,()=>{
+    console.log(`Example app listening on port ${port}`)
+});
+
+
+
